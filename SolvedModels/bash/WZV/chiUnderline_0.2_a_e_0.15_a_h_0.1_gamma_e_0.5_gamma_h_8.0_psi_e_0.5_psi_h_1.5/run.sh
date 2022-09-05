0b7bd9e88d8f6dd17500225a6ac7901dacbdea7a
qhaomin <qhaomin@mcn01.chicagobooth.edu>
2022-09-05 11:45:34 -0500

    1

---
+++ SolvedModels/bash/WZV/chiUnderline_0.2_a_e_0.15_a_h_0.1_gamma_e_0.5_gamma_h_8.0_psi_e_0.5_psi_h_1.5/run.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+#SBATCH --account=pi-lhansen
+#SBATCH --job-name=run
+#SBATCH --output=./job-outs/WZV/chiUnderline_0.2_a_e_0.15_a_h_0.1_gamma_e_0.5_gamma_h_8.0_psi_e_0.5_psi_h_1.5/run.out
+#SBATCH --error=./job-outs/WZV/chiUnderline_0.2_a_e_0.15_a_h_0.1_gamma_e_0.5_gamma_h_8.0_psi_e_0.5_psi_h_1.5/run.err
+#SBATCH --time=0-10:00:00
+#SBATCH --partition=standard
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
+
+module load python/booth/3.8/3.8.5
+
+srun python3 /home/qhaomin/mfrSuite_mercury/SolvedModels/run_mfrSuite.py --chiUnderline 0.2 --a_e 0.15 --a_h 0.1 --gamma_e 0.5 --gamma_h 8.0 --psi_e 0.5 --psi_h 1.5                                                     --nV 30 --nVtilde 0 --V_bar 1.0 --Vtilde_bar 0.0 --sigma_V_norm 0.132 --sigma_Vtilde_norm 0.0 
